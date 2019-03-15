e3f73fbc114863f3f0d80c881d2b478997314ac7
ibuki2003 <ibuki426.sp@gmail.com>
2019-03-15 11:38:03 +0900

    gppにLOCALをdefineするように変更

--- functions.zsh
+++ functions.zsh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 function gpp () {
-    g++ "$1" -o "${1%.*}" -std=c++1y -O2
+    g++ "$1" -o "${1%.*}" -std=c++1y -O2 -DLOCAL
 }
 
 alias 2to3='python "/c/Program Files/Python36/Tools/scripts/2to3.py"'
