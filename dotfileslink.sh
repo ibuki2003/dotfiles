9b5068750a0365310f3f29c8a80ffe43f456e5d8
ibuki2003 <main@fuwa.dev>
2020-08-23 00:06:23 +0900

    neovim: create symlink for init.vim

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 find $(realpath $(dirname $0)/files) -mindepth 1 -maxdepth 1 \
     | xargs -I {} ln -vfs {} ~/
+ln -fs ~/.nvim/init.vim ~/.config/nvim/init.vim # neovim settings
 echo Done.
