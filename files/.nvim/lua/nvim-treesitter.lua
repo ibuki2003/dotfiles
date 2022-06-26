2a13292fc4d647ef63ebda1951faf5d1038f3156
ibuki2003 <main@fuwa.dev>
2022-06-26 22:23:14 +0900

    nvim: remove treesitter disables

--- files/.nvim/lua/nvim-treesitter.lua
+++ files/.nvim/lua/nvim-treesitter.lua
@@ -1,13 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
-    disable = {
-      'lua',
-      'ruby',
-      'toml',
-      'c_sharp',
-      'vue',
-    }
   },
   indent = {
     enable = true,
