41e29504dc9ed3351789adeadc4e9efc4e40a25f
ibuki2003 <main@fuwa.dev>
2022-12-23 05:00:24 +0900

    nvim: add gitsigns

--- files/.nvim/lua/plugins/general.lua
+++ files/.nvim/lua/plugins/general.lua
@@ -235,7 +235,38 @@ return function(packer)
       cmd = { 'G', 'Git' },
     },
     {
-      'airblade/vim-gitgutter',
+      'lewis6991/gitsigns.nvim',
+      config = function()
+        local gs = require('gitsigns')
+        gs.setup {
+          signcolumn = true,
+          numhl = false,
+          attach_to_untracked = true,
+        }
+
+        vim.keymap.set('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        vim.keymap.set('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        vim.keymap.set({'n','v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', { silent = true, noremap = true })
+        vim.keymap.set({'n','v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { silent = true, noremap = true })
+        vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk)
+        vim.keymap.set('n', '<leader>hp', gs.preview_hunk)
+        vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end)
+        vim.keymap.set('n', '<leader>hd', gs.diffthis)
+        vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end)
+        vim.keymap.set('n', '<leader>htd', gs.toggle_deleted)
+        -- Text object
+        vim.keymap.set({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { silent = true, noremap = true })
+      end
     },
     {
       'RRethy/vim-hexokinase',
