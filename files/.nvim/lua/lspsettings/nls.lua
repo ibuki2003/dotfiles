8c9f42c95b3e4b04268d19ffba2e2118f0af3ece
ibuki2003 <main@fuwa.dev>
2023-03-28 15:59:19 +0900

    nvim: update null_ls config

--- files/.nvim/lua/lspsettings/nls.lua
+++ files/.nvim/lua/lspsettings/nls.lua
@@ -3,8 +3,13 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.diagnostics.phpcs,
-    null_ls.builtins.completion.spell,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.code_actions.gitrebase,
+    null_ls.builtins.code_actions.gitsigns,
   },
 })
