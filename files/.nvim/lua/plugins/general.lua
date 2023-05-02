ab4a540cb0b8b4091be13f722b24441895506540
ibuki2003 <main@fuwa.dev>
2023-05-02 23:08:19 +0900

    nvim: fix sandwich recipe

--- files/.nvim/lua/plugins/general.lua
+++ files/.nvim/lua/plugins/general.lua
@@ -38,7 +38,7 @@ return function(packer)
           { buns = {"[ ", " ]"}, nesting = 1, match_syntax = 1, kind = {"add", "replace"}, action = {"add"}, input = {"["} },
           { buns = {"{\\s*", "\\s*}"}, nesting = 1, regex = 1, match_syntax = 1, kind = {"delete", "replace", "textobj"}, action = {"delete"}, input = {"{"} },
           { buns = {"(\\s*", "\\s*)"}, nesting = 1, regex = 1, match_syntax = 1, kind = {"delete", "replace", "textobj"}, action = {"delete"}, input = {"("} },
-          { buns = {"\\[(\\s*", "\\s*\\]"}, nesting = 1, regex = 1, match_syntax = 1, kind = {"delete", "replace", "textobj"}, action = {"delete"}, input = {"["} },
+          { buns = {"\\[\\s*", "\\s*\\]"}, nesting = 1, regex = 1, match_syntax = 1, kind = {"delete", "replace", "textobj"}, action = {"delete"}, input = {"["} },
         }
         vim.g['sandwich#recipes'] = vim.list_extend(vim.g['sandwich#default_recipes'], recipes)
       end,
