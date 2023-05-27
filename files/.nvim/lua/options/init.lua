8ba18c4d799dcebdd35bf9edfc05ca933d53d572
ibuki2003 <main@fuwa.dev>
2023-05-27 15:48:08 +0900

    nvim: check options.local error

--- files/.nvim/lua/options/init.lua
+++ files/.nvim/lua/options/init.lua
@@ -1,4 +1,8 @@
-local _, _ = pcall(require, 'options.local')
+local ok, msg = pcall(require, 'options.local')
+if not ok and not string.match(msg, "module 'options.local' not found") then
+  vim.notify(msg, vim.log.levels.ERROR)
+end
+
 require'options.opts'
 require'options.keymaps'
 require'options.misc'
