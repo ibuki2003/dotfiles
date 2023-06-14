82725c520545327088842683a4160486ce86a3dd
ibuki2003 <main@fuwa.dev>
2023-06-14 19:22:44 +0900

    zsh: update

--- files/.zsh/20-programs.zsh
+++ files/.zsh/20-programs.zsh
@@ -1,5 +1,5 @@
 type vim > /dev/null 2>&1 && export EDITOR=vim
-type nvim > /dev/null 2>&1 && alias vim=nvim && export EDITOR=nvim
+type nvim > /dev/null 2>&1 && alias vim=nvim && export EDITOR=nvim && alias vimdiff='nvim -d'
 
 if type bat > /dev/null; then
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
@@ -7,3 +7,6 @@ elif type batcat > /dev/null; then
   export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
   alias bat=batcat
 fi
+
+# parallel make: use core_count + 1
+export MAKEFLAGS=-j$[$(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g') + 1]" $MAKEFLAGS"
