c240bffdacb85cf9a6441a35f05a4ae306cba7a5
ibuki2003 <main@fuwa.dev>
2023-09-08 03:07:52 +0900

    zsh: improve completion

--- files/.zsh/10-completion.zsh
+++ files/.zsh/10-completion.zsh
@@ -1,14 +1,13 @@
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-# https://superuser.com/questions/415650/does-a-fuzzy-matching-mode-exist-for-the-zsh-shell
-# 1. smart case completion (abc => Abc)
-# 2. word flex completion (abc => A-big-Car)
-# 3. full flex completion (abc => ABraCadabra)
 zstyle ':completion:*' matcher-list \
-  'm:{a-z\-}={A-Z\_}' \
-  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
-  'r:|?=** m:{a-z\-}={A-Z\_}'
+  'm:{[:lower:]}={[:upper:]}' \
+  'r:|?=** m:{[:lower:]}={[:upper:]}'
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*' completer _mycompleter
+
 zstyle ':completion:*' ignore-parents parent pwd ..
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
                    /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
