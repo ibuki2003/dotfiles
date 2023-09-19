9c0b787a2e94588c39e990467b428d816b8bea52
ibuki2003 <main@fuwa.dev>
2023-09-20 02:07:17 +0900

    zsh: update vcs prompt

--- files/.zsh/opts/20-prompt.zsh
+++ files/.zsh/opts/20-prompt.zsh
@@ -10,11 +10,10 @@ zstyle ':vcs_info:git:*' stagedstr "%F{red}+"
 zstyle ':vcs_info:git:*' unstagedstr "%F{red}*"
 zstyle ':vcs_info:*' formats "%F{green}[%b%c%u%F{green}]%f"
 zstyle ':vcs_info:*' actionformats '%F{red}[%b|%a]%f'
-precmd () { vcs_info }
+
 RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
 
 function _update_vcs_info_msg() {
-    LANG=en_US.UTF-8 vcs_info
-    RPROMPT="${vcs_info_msg_0_}"
+    zsh-defer -a +p vcs_info
 }
 add-zsh-hook precmd _update_vcs_info_msg
