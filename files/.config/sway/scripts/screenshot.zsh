986c581fd244e994e47ada1686f54f682db543cf
ibuki2003 <main@fuwa.dev>
2023-12-14 03:46:40 +0900

    sway: show notify on screenshot

--- files/.config/sway/scripts/screenshot.zsh
+++ files/.config/sway/scripts/screenshot.zsh
@@ -10,15 +10,25 @@ copy=0
 
 if [[ -n "${opthash[(i)--select]}" ]]; then
   args+=('-g' "$(slurp)")
+  if [[ $? -ne 0 ]]; then
+    # cancelled
+    exit 1
+  fi
 fi
 
 if [[ -n "${opthash[(i)--clip]}" ]]; then
   args+=('-')
   copy=1
 else
-  args+=(~/Pictures/screenshots/Screenshot_$(date '+%Y%m%d_%H%M%S').png)
+  filename=~/Pictures/Screenshots/Screenshot_$(date '+%Y%m%d_%H%M%S').png
+  args+=($filename)
 fi
 
 grim "${args[@]}" |\
   ( [[ $copy -eq 1 ]] && wl-copy || cat )
 
+if [[ -n "${opthash[(i)--clip]}" ]]; then
+  notify-send -t 3000 "Screenshot copied to clipboard"
+else
+  notify-send -t 3000 "Screenshot saved to $filename"
+fi
