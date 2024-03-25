44da2f6ec2e9dae8c02909c9862d7a5bab9fccd2
ibuki2003 <main@fuwa.dev>
2024-03-25 23:50:24 +0900

    waybar: add mako module

---
+++ files/.config/waybar/scripts/mako.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# argument: toggle
+if [[ "$1" == "toggle" ]]; then
+    if makoctl mode | grep -q 'do-not-disturb'; then
+        makoctl mode -r do-not-disturb > /dev/null
+    else
+        makoctl mode -a do-not-disturb > /dev/null
+    fi
+    exit
+fi
+
+sleep 0.1 # deal with exec-on-event problem (executes script before toggle done)
+if makoctl mode | grep -q 'do-not-disturb'; then
+    # muted icon
+    echo '󰂛'
+else
+    # bell icon
+    echo '󰂞'
+fi
