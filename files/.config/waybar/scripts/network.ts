60c03c6b3eec61ff71df8eed70e5181617a03257
ibuki2003 <main@fuwa.dev>
2024-05-22 19:00:01 +0900

    waybar: update network module

--- files/.config/waybar/scripts/network.ts
+++ files/.config/waybar/scripts/network.ts
@@ -1,29 +1,37 @@
 const decode = (s: Uint8Array) => new TextDecoder().decode(s);
 
-async function get_interfaces(): Promise<string[]> {
-  const p = await new Deno.Command("find", {
-    args: [
-      "/sys/class/net",
-      "-mindepth", "1",
-      "-maxdepth", "1",
-      "-lname", "*virtual*",
-      "-prune",
-      "-o",
-      "-printf", "%f\n",
-    ],
-    stdout: "piped",
-  }).output();
-  return decode(p.stdout).trim().split("\n");
-}
+const NmAvailable = true;
 
-type Rec = Record<string, string | number>;
+async function get_ifs() {
+  let fields = [];
 
-async function get_ifconfig(): Promise<Rec[]> {
-  const p = await new Deno.Command("jc", {
-    args: ["ifconfig"],
-    stdout: "piped",
-  }).output();
-  return JSON.parse(decode(p.stdout));
+  if (NmAvailable) {
+    const p = decode(
+      (await new Deno.Command("nmcli", {
+        stdout: "piped",
+      }).output()).stdout,
+    )
+      .replaceAll(/^\troute.*\n/gm, "")
+      .replaceAll("\t", "  ");
+
+    fields = p.split("\n\n")
+      .filter((l) => l.match(/^\w+: connected/));
+  } else {
+    const p = decode(
+      (await new Deno.Command("ifconfig", {
+        stdout: "piped",
+      }).output()).stdout,
+    )
+      .replaceAll(/^.+packets.*\n/gm, "")
+      .replaceAll(/^ {8}/gm, "  ");
+
+    fields = p.split("\n\n").filter(Boolean);
+  }
+
+  return Object.fromEntries(fields.map((f) => {
+    const name = f.split(":", 1)[0];
+    return [name, f];
+  }));
 }
 
 async function get_primary_if(): Promise<string | null> {
@@ -38,31 +46,54 @@ async function get_primary_if(): Promise<string | null> {
   return m ? m[1] : null;
 }
 
-async function get_iwconfig(): Promise<Rec[]> {
-  const p = await new Deno.Command("jc", {
-    args: ["iwconfig"],
-    stdout: "piped",
-  }).output();
-  if (!p.success) return [];
+type Rec = Record<string, string>;
 
-  return JSON.parse(decode(p.stdout));
+function unquote(s: string): string {
+  const r = /^"(.*)"$/.exec(s);
+  return r ? r[1] : s;
 }
 
-async function get_usage(): Promise<Record<string, [number, number]>> {
+async function get_iwconfig(): Promise<Record<string, Rec>> {
+  const p = await new Deno.Command("iwconfig", { stdout: "piped" }).output();
+  if (!p.success) return {};
+  const o = decode(p.stdout);
+
+  const entries = o.split("\n\n").filter(Boolean)
+    .map((e) => {
+      const [name, protocol, ...fields] = e.split(/ {2,}|\n/).filter(Boolean);
+      const f = Object.fromEntries(fields.map((l) => {
+        const i = /[:=]/.exec(l)?.index;
+        if (!i) return [];
+        return [
+          l.slice(0, i).trim().toLowerCase().replace(/[ -]/g, "_"),
+          unquote(l.slice(i + 1).trim()),
+        ];
+
+      }));
+      f.protocol = protocol;
+      return [name, f];
+    });
+
+  return Object.fromEntries(entries);
+}
+
+async function get_usage(): Promise<Speeds> {
   const t = decode(await Deno.readFile("/proc/net/dev"));
-  return Object.fromEntries(t.split("\n")
-    .map(l => l.split(':'))
-    .filter(l => l.length == 2)
-    .map(([name, s]) => {
-      const f = s.split(/\s+/).filter(Boolean);
-      return [
-        name,
-        [
-          parseInt(f[0]) as number,
-          parseInt(f[8]) as number,
-        ],
-      ]
-    }));
+  return Object.fromEntries(
+    t.split("\n")
+      .map((l) => l.trim().split(":"))
+      .filter((l) => l.length == 2)
+      .map(([name, s]) => {
+        const f = s.split(/\s+/).filter(Boolean);
+        return [
+          name,
+          [
+            parseInt(f[0]) as number,
+            parseInt(f[8]) as number,
+          ],
+        ];
+      }),
+  );
 }
 
 const LABELS = ["B", "K", "M", "G", "T"];
@@ -80,90 +111,67 @@ function has<X, Y extends PropertyKey>(
   x: X,
   y: Y,
 ): x is X & Record<Y, unknown> {
+  if (typeof x !== "object" || x === null) return false;
   return Object.prototype.hasOwnProperty.call(x, y);
 }
 
-interface IfInfo {
-  ifc: {
-    ipv4: { address: string }[] | null;
-    ipv6: { address: string }[] | null;
-    [key: string]: unknown;
-  };
-
-  iwc?: {
-    signal_level: number;
-    [key: string]: unknown;
-  };
-};
-
-type Ifs = Record<string, IfInfo>;
 type Speeds = Record<string, [number, number]>;
 
-async function update(ifs: Ifs, speeds: Speeds, primary_if: string | null) {
-  const tooltip = Object.keys(ifs)
-    .filter((i) => speeds[i])
-    .map((i) => {
-      const info = ifs[i];
-      const spd = speeds[i];
-      let text = i;
-      if (info.iwc) {
-        // wifi
-        text +=
-          ` ${info.iwc.essid} ${info.iwc.frequency}${info.iwc.frequency_unit}\n`;
-        if (info.iwc.signal_level)
-          text += `${info.iwc.signal_level}${info.iwc.signal_level_unit} `;
-        if (info.iwc.bit_rate)
-          text += `${info.iwc.bit_rate}${info.iwc.bit_rate_unit}\n`;
-      } else {
-        text += "\n";
-      }
-      text += `${format_size(spd[1])} / ${format_size(spd[0])}\n`;
+async function update(
+  ifs: Record<string, string>,
+  iw_info: Record<string, Rec>,
+  primary_if: string | null,
+  speeds: Speeds,
+) {
+  const tooltip = Object.keys(ifs).map((i) => {
+    let text = ifs[i] + "\n";
+    const speed = (has(speeds, i) ? speeds[i] : null) ?? [0, 0];
 
-      if (info.ifc.ipv4) {
-        text += info.ifc.ipv4
-          .map((ip: { address: string }) => ` ${ip.address}`)
-          .join("\n");
-        text += '\n';
-      }
-      if (info.ifc.ipv6) {
-        text += info.ifc.ipv6
-          .map((ip: { address: string }) => ` ${ip.address}`)
-          .join("\n");
-        text += '\n';
-      }
+    if (has(iw_info, i)) {
+      const iwc = iw_info[i];
 
-      return text;
-    })
+      // wifi
+      text += `  ${iwc.essid} ${iwc.frequency}\n`;
+      if (iwc.signal_level) {
+        text += `  ${iwc.signal_level} `;
+      }
+      if (iwc.bit_rate) {
+        text += `  ${iwc.bit_rate}\n`;
+      }
+    }
+    text += `  ${format_size(speed[1])} / ${format_size(speed[0])}\n`;
+
+    return text;
+  })
     .join("\n\n");
 
-  const p = primary_if ? ifs[primary_if] : null;
-  const s = primary_if ? speeds[primary_if] ?? [0, 0] : [0, 0];
+  // TODO
+  let text = "No connection";
+  let mode = "nc";
+  let percentage = 0;
 
-  const text = p
-    ? `${
-      p.iwc?.essid ?? p.ifc.ipv4_addr
-    } ${
-      format_size(s[1])
-    } / ${
+  if (primary_if) {
+    const p = ifs[primary_if];
+    const s = speeds[primary_if] ?? [0, 0];
+    const l = p.split("\n").map((l) => l.trim());
+
+    const is_wifi = l[2].startsWith("wifi ");
+
+    const name = (is_wifi
+      ? /: connected to (.+?)$/.exec(l[0])?.[1]
+      : l.filter((l) =>
+        l.startsWith("inet")
+      )[0]) ?? "Unknown";
+
+    text = `${name} ${format_size(s[1])} / ${
       format_size(s[0])
-    }`
-    : "No connection";
+    }`;
 
+    mode = is_wifi ? "wifi" : "eth";
 
-  const mode = primary_if
-    ? has(p, "iwc")
-      ? "wifi"
-      : "eth"
-    : "nc";
-
-  const percentage = p?.iwc ?
-    Math.round(
-      Math.min(
-          Math.max(1 - (-45 - p.iwc.signal_level) / 45, 0),
-          1,
-        ) * 100
-    )
-    : 0;
+    const iw = has(iw_info, primary_if) ? iw_info[primary_if] : null;
+    percentage = link_quality_percentage(iw?.link_quality);
+  }
 
   await Deno.stdout.write(
     new TextEncoder().encode(
@@ -177,78 +185,83 @@ async function update(ifs: Ifs, speeds: Speeds, primary_if: string | null) {
   );
 }
 
-let last_usage: Record<string, [number, number]> = {};
-async function get_speeds(interval: number): Promise<Speeds> {
-  const ret: Speeds = {};
-  const a = await get_usage();
-  Object.keys(a).forEach((i) => {
-    if (has(last_usage, i)) {
-      ret[i] = [
-        (a[i][0] - last_usage[i][0]) / interval,
-        (a[i][1] - last_usage[i][1]) / interval,
-      ];
+function link_quality_percentage(quality: string | undefined): number {
+  if (!quality) return 0;
+  if (!quality.includes("/")) return 0;
+  const f = quality.split("/");
+  return Math.round(parseInt(f[0]) / parseInt(f[1]) * 100);
+}
+
+async function* watch_nmcli() {
+  while (true) {
+    const p = new Deno.Command("nmcli", {
+      args: ["monitor"],
+      stdout: "piped",
+    }).spawn().stdout.getReader();
+
+    while (true) {
+      const n = await p.read();
+      if (n.value) yield 1;
+      if (n.done) break;
     }
-  });
-  last_usage = a;
-  return ret;
+  }
 }
 
-async function get_infos(): Promise<Ifs> {
-  const ret: Ifs = {};
-  await get_interfaces().then((interfaces) => {
-    for (const i of interfaces) {
-      ret[i] = {
-        ifc: {
-          ipv4: [],
-          ipv6: [],
-        },
-      };
-    }
-  });
-  await Promise.all([
-    get_iwconfig().then((w) => {
-      w.forEach(i => {
-        if (has(ret, i.name)) {
-          ret[i.name].iwc = i as IfInfo['iwc'];
-        }
-      });
-    }),
-    get_ifconfig().then((a) => {
-      a.forEach((i) => {
-        if (has(ret, i.name)) {
-          ret[i.name].ifc = i as IfInfo['ifc'];
-        }
-      });
-    }),
-  ]);
-  return ret;
+async function* usage_speed() {
+  let last_usage: Speeds = {};
+  let last_t = +new Date();
+
+  while (true) {
+    const usage = await get_usage();
+    const ret: Speeds = {};
+    let itv = (+new Date() - last_t) / 1000;
+    if (itv === 0) itv = 0.1;
+    Object.keys(usage).forEach((i) => {
+      if (has(last_usage, i)) {
+        ret[i] = [
+          (usage[i][0] - last_usage[i][0]) / itv,
+          (usage[i][1] - last_usage[i][1]) / itv,
+        ];
+      }
+    });
+    last_usage = usage;
+    last_t = +new Date();
+    yield ret;
+  }
 }
 
-function sleep(duration: number) {
-  return new Promise((resolve) => setTimeout(resolve, duration));
+async function* interval(t: number) {
+  while (true) {
+    yield 1;
+    await delay(t);
+  }
 }
 
-const interval = 3; // seconds
-const info_itv = 30; // times
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function main() {
-  let infos: Ifs = {};
+  let infos: Record<string, string> = {};
   let primary_if: string | null = null;
   let speeds: Speeds = {};
+  let iw_info: Record<string, Rec> = {};
 
-  let c = 0;
+  const watch = watch_nmcli();
+  const watch_speed = usage_speed();
+
+  const itv = interval(5000);
+
   while (true) {
-    speeds = await get_speeds(interval);
-
-    const need_update = Object.keys(speeds).some((i) => infos[i]);
-    if (c-- <= 0 || need_update) {
-      primary_if = await get_primary_if();
-      infos = await get_infos();
-      c = info_itv;
-    }
-    await update(infos, speeds, primary_if);
-
-    await sleep(interval * 1000);
+    await Promise.any([
+      watch.next().then(async () => {
+        infos = await get_ifs();
+        primary_if = await get_primary_if();
+      }),
+      itv.next().then(async () => {
+        iw_info = await get_iwconfig();
+        speeds = await watch_speed.next().then((x) => x.value) ?? {};
+      }),
+    ]);
+    update(infos, iw_info, primary_if, speeds);
   }
 }
 
