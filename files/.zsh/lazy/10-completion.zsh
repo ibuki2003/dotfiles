070437f483c83b5320800b64c232efec1d6bdb41
ibuki2003 <main@fuwa.dev>
2024-07-04 17:42:56 +0900

    zsh: improve completion

--- files/.zsh/lazy/10-completion.zsh
+++ files/.zsh/lazy/10-completion.zsh
@@ -6,8 +6,12 @@ zstyle ':completion:*' matcher-list \
   'r:|?=** m:{[:lower:]}={[:upper:]}'
 
 zstyle ':completion:*' menu select search
-zstyle ':completion:*' completer _prefix _mycompleter
+zstyle ':completion:*' expand prefix
+zstyle ':completion:*' completer _expand _prefix _mycompleter
 
 zstyle ':completion:*' ignore-parents parent pwd ..
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
                    /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "${XDG_CACHE_HOME}/zsh/compcache"
