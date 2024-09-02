3669c64abeb0990033e312c433acfcf9606e0c7e
ibuki2003 <main@fuwa.dev>
2024-09-02 10:05:49 +0900

    zsh: fix fuzzy completion

--- files/.zsh/lazy/10-completion.zsh
+++ files/.zsh/lazy/10-completion.zsh
@@ -1,13 +1,12 @@
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-zstyle ':completion:*' matcher-list \
-  'm:{[:lower:]}={[:upper:]}' \
-  'r:|?=** m:{[:lower:]}={[:upper:]}'
+zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}'
+zstyle ':completion:*:fuzzy:*:*:*' matcher-list 'r:|?=** m:{[:lower:]}={[:upper:]}'
 
 zstyle ':completion:*' menu select search
 zstyle ':completion:*' expand prefix
-zstyle ':completion:*' completer _expand _prefix _mycompleter
+zstyle ':completion:*' completer _expand _prefix _complete _mycompleter:fuzzy _list
 
 zstyle ':completion:*' ignore-parents parent pwd ..
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
