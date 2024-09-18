954fbe42b2536510024774f599e6c6967a430d30
ibuki2003 <main@fuwa.dev>
2024-09-19 03:26:33 +0900

    nvim: update keymaps

--- files/.nvim/lua/options/keymaps.lua
+++ files/.nvim/lua/options/keymaps.lua
@@ -2,40 +2,28 @@
 vim.g.mapleader = " "
 
 -- wrapped move
-vim.keymap.set({'n','v'}, 'j', 'gj', { noremap = true, silent = true })
-vim.keymap.set({'n','v'}, 'k', 'gk', { noremap = true, silent = true })
-vim.keymap.set({'n','v'}, 'gj', 'j', { noremap = true, silent = true })
-vim.keymap.set({'n','v'}, 'gk', 'k', { noremap = true, silent = true })
+vim.keymap.set({'n','v'}, 'j', 'gj', { silent = true })
+vim.keymap.set({'n','v'}, 'k', 'gk', { silent = true })
+vim.keymap.set({'n','v'}, 'gj', 'j', { silent = true })
+vim.keymap.set({'n','v'}, 'gk', 'k', { silent = true })
 
-vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohlsearch\n', { noremap = true })
+vim.keymap.set('n', '<Esc><Esc>', '<Cmd>nohlsearch<CR>')
 
--- move between split
-vim.api.nvim_set_keymap('n', '<C-j>', ':bnext<CR>', { noremap = true, silent = true })
--- nnoremap <silent><C-k> :bprev<CR>
-vim.api.nvim_set_keymap('n', '<C-k>', ':bprev<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<Cmd>bnext<CR>', { silent = true })
+vim.keymap.set('n', '<C-k>', '<Cmd>bprev<CR>', { silent = true })
 
--- block indent
-vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
-vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
-vim.keymap.set('n', '<Space>o', function()
-  for _ = 1, vim.v.count1 do
-    vim.fn.append(vim.fn.line('.'), '')
-  end
-end, { noremap = true, silent = true})
+vim.keymap.set('n', '<Space>o', function() for _ = 1, vim.v.count1 do vim.fn.append(vim.fn.line('.'), '') end end, { silent = true })
+vim.keymap.set('n', '<Space>O', function() for _ = 1, vim.v.count1 do vim.fn.append(vim.fn.line('.')-1, '') end end, { silent = true })
 
-vim.keymap.set('n', '<Space>O', function()
-  for _ = 1, vim.v.count1 do
-    vim.fn.append(vim.fn.line('.')-1, '')
-  end
-end, { noremap = true, silent = true})
+vim.keymap.set({'n','v'}, '<Leader>y', '"+y', { silent = true })
+vim.keymap.set({'n','v'}, '<Leader>d', '"+d', { silent = true })
+vim.keymap.set({'n','v'}, '<Leader>p', '"+p', { silent = true })
+vim.keymap.set({'n','v'}, '<Leader>P', '"+P', { silent = true })
 
-vim.keymap.set({'n','v'}, '<Leader>y', '"+y', { noremap = true, silent = true })
-vim.keymap.set({'n','v'}, '<Leader>d', '"+d', { noremap = true, silent = true })
-vim.keymap.set({'n','v'}, '<Leader>p', '"+p', { noremap = true, silent = true })
-vim.keymap.set({'n','v'}, '<Leader>P', '"+P', { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('n', '<C-g>', 'ggVG', { noremap = true })
+vim.keymap.set('n', '<C-g>', 'ggVG')
 
 vim.api.nvim_set_keymap('n', '0', '', {
   noremap = true,
@@ -51,11 +39,20 @@ vim.api.nvim_set_keymap('n', '0', '', {
   end,
 })
 
-vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true })
-vim.api.nvim_set_keymap('n', 'X', '"_X', { noremap = true })
+vim.keymap.set('n', 'x', '"_x')
+vim.keymap.set('n', 'X', '"_X')
+
+vim.keymap.set({'o', 'x'}, 'i<space>', 'iW')
+vim.keymap.set('n', 'M', '%')
+
+vim.keymap.set('n', 'i', function() if vim.fn.getline('.') == '' then return '"_cc' else return 'i' end end, { expr = true })
+vim.keymap.set('n', 'A', function() if vim.fn.getline('.') == '' then return '"_cc' else return 'A' end end, { expr = true })
+
+vim.keymap.set('n', '/', '/\\v')
+vim.keymap.set('n', '?', '/\\V')
 
 -- terminal escape
-vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 
 
 -- https://zenn.dev/vim_jp/articles/20230814_ekiden_fullpath
@@ -77,19 +74,17 @@ vim.keymap.set({'c','i'}, '<C-p>', expandpath, { noremap = true, expr = true })
 
 vim.keymap.set("n", "gf", function()
   local cfile = vim.fn.expand("<cfile>")
-  print("nyan", cfile)
   if cfile:match("^https?://") then
-    -- Neovim nightlyなら `vim.ui.open(cfile)` が便利。
-    vim.fn.system({"xdg-open", cfile})
+    vim.ui.open(cfile)
   else
     vim.cmd("normal! gF")
   end
 end)
 
 -- https://blog.atusy.net/2024/05/29/vim-hl-enhanced/
-vim.api.nvim_set_keymap('n', 'H', 'H<Plug>(H)', { noremap = true })
-vim.api.nvim_set_keymap('n', 'L', 'L<Plug>(L)', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Plug>(H)H', '<C-u>H<Plug>(H)', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Plug>(L)L', '<C-d>Lzb<Plug>(L)', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Plug>(H)', '<Nop>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Plug>(L)', '<Nop>', { noremap = true })
+vim.keymap.set('n', 'H', 'H<Plug>(H)')
+vim.keymap.set('n', 'L', 'L<Plug>(L)')
+vim.keymap.set('n', '<Plug>(H)H', '<C-u>H<Plug>(H)')
+vim.keymap.set('n', '<Plug>(L)L', '<C-d>Lzb<Plug>(L)')
+vim.keymap.set('n', '<Plug>(H)', '<Nop>')
+vim.keymap.set('n', '<Plug>(L)', '<Nop>')
--- files/.nvim/lua/plugins/general.lua
+++ files/.nvim/lua/plugins/general.lua
@@ -395,15 +395,6 @@ return {
         end
       end,
     },
-    init = function()
-      vim.keymap.set("n", "gf", function()
-        if require("obsidian").util.cursor_on_markdown_link() then
-          return "<cmd>ObsidianFollowLink<CR>"
-        else
-          return "gf"
-        end
-      end, { noremap = false, expr = true })
-    end,
   },
   {
     'tani/dmacro.nvim',
