3c3410f63320bdab85413f6eff9086881a289c7c
ibuki2003 <main@fuwa.dev>
2024-12-09 22:44:19 +0900

    nix: setup nixos

--- flake.nix
+++ flake.nix
@@ -2,12 +2,7 @@
   description = "fuwa dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    neovim-nightly-overlay.url ="github:nix-community/neovim-nightly-overlay";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = {
@@ -21,33 +16,11 @@
     in
   {
 
-    apps.${system} = {
-      update = {
-        type = "app";
-        program = toString (pkgs.writeShellScript "update-script" ''
-          set -e
-          nix flake update
-          nix run 'nixpkgs#home-manager' -- switch --flake '.#myHomeConfig'
-        '');
-      };
-
-      install = {
-        type = "app";
-        program = toString (pkgs.writeShellScript "update-script" ''
-          set -e
-          nix run 'nixpkgs#home-manager' -- switch --flake '.#myHomeConfig'
-        '');
-      };
-    };
-
-    homeConfigurations = {
-      myHomeConfig = home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs;
-        extraSpecialArgs = {
-          inherit inputs;
-        };
+    nixosConfigurations = {
+      myNixOS = inputs.nixpkgs.lib.nixosSystem {
+        system = system;
         modules = [
-          ./home.nix
+          ./configuration.nix
         ];
       };
     };
