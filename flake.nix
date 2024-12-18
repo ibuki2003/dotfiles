4eff184446a1152d999c66a4c0aa5d617fa272c1
ibuki2003 <main@fuwa.dev>
2024-12-19 03:31:46 +0900

    nix: fix redundant input dependency

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
