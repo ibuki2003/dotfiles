592091b3fc50cf844345d2850b2d2fba650352bd
ibuki2003 <main@fuwa.dev>
2025-06-10 01:25:22 +0900

    nvim: setup codecompanion (again!)

--- files/.nvim/lua/plugins/ai.lua
+++ files/.nvim/lua/plugins/ai.lua
@@ -12,11 +12,80 @@ return {
     },
   },
   {
-    'madox2/vim-ai',
-    init = function()
-      vim.g.vim_ai_roles_config_file = vim.fn.expand("~/.nvim/vimai/roles.ini")
-      vim.g.vim_ai_complete = { options = { selection_boundary = '```', }, }
-      vim.g.vim_ai_edit = { options = { selection_boundary = '```', }, }
+    'olimorris/codecompanion.nvim',
+    cmd = {
+      "CodeCompanion",
+      "CodeCompanionCmd",
+      "CodeCompanionChat",
+      "CodeCompanionActions",
+      "CodeCompanionAutoSaveChat",
+      "CodeCompanionHistory",
+    },
+    keys = {
+      { "<Space>cc", ":CodeCompanionChat Toggle<CR>", mode = { "n" }, }, -- Just show the chat
+      { "<Space>ca", ":CodeCompanion<CR>", mode = { "n", "x" }, },
+      { "<Space>cA", ":CodeCompanionActions<CR>", mode = { "n", "x" }, },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "openai",
+          keymaps = {
+            clear = { modes = { n = {} } }, -- undef
+          },
+        },
+        inline = { adapter = "openai" },
+      },
+      display = {
+        action_palette = { provider = "telescope" },
+        chat = {
+          auto_scroll = false,
+
+          show_header_separator = true,
+          -- show_settings = true, -- NOTE: enabling this blocks adapter/model selection ui
+        },
+      },
+      adapters = {
+        opts = {
+          show_defaults = false,
+          show_model_choices = true,
+        },
+        copilot = 'copilot',
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            schema = {
+              model = {
+                default = "gpt-4.1-mini",
+                -- HACK: replace the table, not merge
+                choices = function() return {
+                  ["gpt-4.1"] = { opts = { has_vision = true } },
+                  ["gpt-4.1-mini"] = { opts = { has_vision = true } },
+                  ["gpt-4.1-nano"] = { opts = { has_vision = true } },
+                  ["o3"] = { opts = { has_vision = true, can_reason = true } },
+                  ["o4-mini"] = { opts = { has_vision = true, can_reason = true } },
+                  ["o1"] = { opts = { has_vision = true, can_reason = true } },
+                } end,
+              },
+              reasoning_effort = {
+                -- fix condition
+                condition = function(self)
+                  local model = self.schema.model.default
+                  return model:sub(1, 1) == "o"
+                end,
+              },
+            },
+            env = { api_key = "cmd:secret-tool lookup service openai" },
+          })
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("settings.codecompanion.spinner"):init()
+      require("codecompanion").setup(opts)
     end,
   },
 }
