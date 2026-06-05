9b8cfd8fbb611025e836debb9ec6f6554eb19b00
ibuki2003 <main@fuwa.dev>
2026-06-06 06:47:22 +0900

    fix: dotfileslink .claude

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -17,18 +17,26 @@ function ln_checked() {
 }
 
 function ln_files() {
-    if [ ! -e ~/"$1" ]; then
-        mkdir -p ~/"$1"
-    fi
+  if [ ! -e ~/"$1" ]; then
+    mkdir -p ~/"$1"
+  fi
 
-    for filename in $(find "$root/files/$1" -mindepth 1 -maxdepth 1 | xargs -n1 basename); do
-        fn="$1/$filename"
-        if [[ $filename = .config ]]; then
-            ln_files $filename
-        else
-            ln_checked "$root/files/$fn" ~/"$fn"
-        fi
-    done
+  for filename in $(find "$root/files/$1" -mindepth 1 -maxdepth 1 | xargs -n1 basename); do
+    fn="$1/$filename"
+    # if [[ $filename = .config ]]; then
+    #     ln_files $filename
+    # else
+    #     ln_checked "$root/files/$fn" ~/"$fn"
+    # fi
+    case $filename in
+      .config | .claude)
+        ln_files $filename
+        ;;
+      *)
+        ln_checked "$root/files/$fn" ~/"$fn"
+        ;;
+    esac
+  done
 }
 
 ln_files .
